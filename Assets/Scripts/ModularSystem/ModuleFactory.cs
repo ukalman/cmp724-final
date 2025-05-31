5f18481aafc7fef979aaf40325431619d79a93d2
ukalman <umutkalman@hotmail.com>
2025-05-31 20:30:05 +0300

    Update ModuleFactory.cs

--- Assets/Scripts/ModularSystem/ModuleFactory.cs
+++ Assets/Scripts/ModularSystem/ModuleFactory.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public static class ModuleFactory
 {
     public static IModuleBase CreateModule(ModuleEntry entry)
@@ -5,15 +7,15 @@ public static class ModuleFactory
         switch (entry.type)
         {
             case ModuleEntry.ModuleType.Health:
-                var healthConfig = entry.config as HealthModuleConfig;
-                return new HealthModule(healthConfig);
+                //var healthConfig = entry.config as HealthModuleConfig;
+                //return new HealthModule(healthConfig);
 
             case ModuleEntry.ModuleType.PlayerMovement:
-                var moveConfig = entry.config as PlayerMovementConfig;
-                return new PlayerMovementModule(moveConfig);
+                //var moveConfig = entry.config as PlayerMovementConfig;
+                //return new PlayerMovementModule(moveConfig);
 
             case ModuleEntry.ModuleType.Combat:
-                return new CombatModule(); // Henüz config almıyor diyelim
+                //return new CombatModule(); 
 
             default:
                 Debug.LogError($"Unsupported module type: {entry.type}");
